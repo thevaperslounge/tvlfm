0642ae056bdb596342a9e82499d7ec92f73dc6c9
Martin Page <wookoouk@gmail.com>
2014-08-20 20:58:48 +0100

    live version

--- mail.php
+++ mail.php
@@ -4,7 +4,7 @@ $email = $_POST['email'];
 $phone = $_POST['phone'];
 $message = $_POST['message'];
 $formcontent="From: $name \n Message: $message \n Tel: $phone";
-$recipient = "wookoouk@gmail.com";
+$recipient = "website@thevapersloungefm.co.uk";
 $subject = "Contact Form";
 $mailheader = "From: $email \r\n";
 mail($recipient, $subject, $formcontent, $mailheader) or die("Error!");
